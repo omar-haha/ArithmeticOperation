1c8952c9dd33de983a40f308c8a2c0b52bc044bc
omar-haha <125899641+omar-haha@users.noreply.github.com>
2025-04-06 14:31:23 -0400

    Update ArithmeticOperationsTest.java
    make build fail

--- src/test/java/soen/tutorial/ArithmeticOperationsTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationsTest.java
@@ -14,7 +14,6 @@ public class ArithmeticOperationsTest {
 		ArithmeticOperations operations = new ArithmeticOperations();
 		Integer actual = operations.addOrSub(2, 6);
 		Integer expected = 8;
-		assertEquals(expected, actual);
 
 	}
 /*    @Test
